3d467c5e50f53df71e54ad69f30a361c939f7633
jenni <teosjennifer2412@gmail.com>
2025-06-19 09:51:46 -0600

    cambio arreglo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'; // Imports the React library, necessary for creating React components.
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Imports components from react-router-dom for handling routing.
-import Home from './pages/Home'; // Imports the Home page component.
-import AboutUs from './pages/AboutUs'; // Imports the About Us page component.
-import Products from './pages/Products'; // Imports the Products page component.
-import Profile from './pages/Profile'; // Imports the Profile page component.
-import './App.css'; // Imports the global stylesheet for the application.
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -23,7 +14,6 @@ import Hitorial from './pages/Historial';
 
 
 
->>>>>>> master
 
 // This is the main component of the application, which acts as a container for all other components.
 function App() {
@@ -33,16 +23,6 @@ function App() {
     <Router>
       {/* The Routes component is a container for a collection of Route components. */}
       <Routes>
-<<<<<<< HEAD
-        {/* Each Route component maps a URL path to a specific component. */}
-        <Route path="/" element={<Home />} /> {/* This route maps the root URL to the Home component. */}
-        <Route path="/sobre-nosotros" element={<AboutUs />} /> {/* This route maps the /sobre-nosotros URL to the AboutUs component. */}
-        <Route path="/products" element={<Products />} /> {/* This route maps the /products URL to the Products component. */}
-        <Route path="/profile" element={<Profile />} /> {/* This route maps the /profile URL to the Profile component. */}
-        {/* This is a commented-out example of how to define other routes. */}
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/products" element={<Products />} /> */}
-=======
 
         <Route path="/" element={<Home />} />
         <Route path="/recuperacion" element={<Recuperacion />} />
@@ -60,7 +40,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} /> 
         */}
->>>>>>> master
       </Routes>
     </Router>
   );
